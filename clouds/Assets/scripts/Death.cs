5c4da6dc78a4a8c252bf874a29c5f98ded813c7b
hayes <hayes.g850@gmail.com>
2019-05-28 12:09:45 -0600

    adding platform waypoints

---
+++ clouds/Assets/scripts/Death.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    public Transform respawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Death")
+        {
+            transform.position = new Vector2(respawn.position.x, respawn.position.y);
+        }
+    }
+}
---
+++ clouds/Assets/scripts/camerafollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    public bool isfollowing = true;
+    public Transform player;
+    public Transform Camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isfollowing = true)
+        {
+            transform.position = new Vector3(player.position.x, Camera.position.y, Camera.position.z);
+        }
+    }
+}
